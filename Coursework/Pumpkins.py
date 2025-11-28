0c2a502eca76c9a8b7eb2e7228feead27e113325
laylameghjee <mbxlm9@nottingham.ac.uk>
2025-11-28 16:06:58 +0000

    adding the write up word doc file and filtered csv file

--- Coursework/Pumpkins.py
+++ Coursework/Pumpkins.py
@@ -15,8 +15,8 @@ pumpkins = pd.read_csv('pumpkins_01.csv')
 
 ##      TASK TWO
 # identifying the heaviest pumpkin.
-heaviest = pumpkins.sort_values("weight_lbs", ascending=False)
-print("Below is the heaviest pumpkin")
+heaviest = pumpkins.sort_values('weight_lbs', ascending=False)
+print('Below is the heaviest pumpkin')
 # prints only selected columns from the data set
 print(heaviest.iloc[[0]][['id','weight_lbs', 'city', 'country', 'variety' ]])
 
@@ -27,39 +27,42 @@ def convert(weight_lbs):
 	return weight_lbs * 0.453592
 
 # creating a new column to add in kilo values
-pumpkins["weight_kg"] = convert(pumpkins["weight_lbs"])
+pumpkins['weight_kg'] = convert(pumpkins['weight_lbs'])
 
 
 ##      TASK FOUR
 # adding a weight, class column with three categories
 def classw(w):
 	if w < 500:
-		return "light"
+		return 'light'
 	elif w < 1000:
-		return "medium"
+		return 'medium'
 	else:
-		return "heavy"
+		return 'heavy'
 
 # creating a new column to add in values for weight class
 # using apply so the function runs on each individual value to avoid errors
-pumpkins["weight_class"] = pumpkins["weight_lbs"].apply(classw)	
+pumpkins['weight_class'] = pumpkins['weight_lbs'].apply(classw)	
 
 
 ##      TASK FIVE
 #plot estimated v actual weight and coloured by class
-category = pumpkins["weight_class"]
+# ADD A KEY OR SOMEHTHING??
+category = pumpkins['weight_class']
 colour_options = {'light': 'green', 'medium': 'orange', 'heavy': 'red'}
 colors = category.map(colour_options)
-plt.scatter(pumpkins["est_weight"], pumpkins["weight_lbs"], c=colors, edgecolor="black")
-plt.xlabel("estimated weight(lbs)")
-plt.ylabel("actual weight(lbs)")
-plt.title("Scatter to show estimated weight v actual weight")
+plt.scatter(pumpkins['est_weight'], pumpkins['weight_lbs'], c=colors, edgecolor='black')
+plt.xlabel('estimated weight(lbs)')
+plt.ylabel('actual weight(lbs)')
+plt.title('Scatter to show estimated weight v actual weight')
 plt.show() 
 
 
 ##      TASK FIVE
 #filter for three countries and save (pumpkins_filtered.csv)
-
+countries = ['Italy', 'USA', 'Austria']
+pumpkins_filtered = pumpkins[pumpkins['country'].isin(countries)]
+pumpkins_filtered.to_csv('pumpkins_filtered.csv')
 
 #summarise mean weights by country and variety 
 
