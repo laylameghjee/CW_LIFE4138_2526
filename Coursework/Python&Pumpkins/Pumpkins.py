9fd929710c5ebe7225e317ad7f21efbc2b477d0e
laylameghjee <mbxlm9@nottingham.ac.uk>
2025-12-04 16:50:46 +0000

    adding completed pumpkins task

--- Coursework/Python&Pumpkins/Pumpkins.py
+++ Coursework/Python&Pumpkins/Pumpkins.py
@@ -1,12 +1,14 @@
 # # # # # # # # # # # # # # # # # # # #
 #        Pumpkins Challenge           #
 #           Layla Meghjee             #
-#           November 2025             #
+#           December 2025             #
+#            LIFE4138 CW              #
 # # # # # # # # # # # # # # # # # # # #
 
 # importing libraries 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 ##      TASK ONE
 # reading in csv file
@@ -18,7 +20,7 @@ pumpkins = pd.read_csv('pumpkins_01.csv')
 heaviest = pumpkins.sort_values('weight_lbs', ascending=False)
 print('Below is the heaviest pumpkin')
 # prints only selected columns from the data set
-print(heaviest.iloc[[0]][['id','weight_lbs', 'city', 'country', 'variety' ]])
+print(heaviest.iloc[[0]][['id','weight_lbs', 'city', 'country', 'variety']])
 
 
 ##      TASK THREE
@@ -47,7 +49,6 @@ pumpkins['weight_class'] = pumpkins['weight_lbs'].apply(classw)
 
 ##      TASK FIVE
 #plot estimated v actual weight and coloured by class
-# ADD A KEY OR SOMEHTHING??
 category = pumpkins['weight_class']
 colour_options = {'light': 'green', 'medium': 'orange', 'heavy': 'red'}
 colors = category.map(colour_options)
@@ -57,17 +58,47 @@ plt.ylabel('actual weight(lbs)')
 plt.title('Scatter to show estimated weight v actual weight')
 plt.show() 
 
-
-##      TASK FIVE
+##      TASK SIX
 #filter for three countries and save (pumpkins_filtered.csv)
-countries = ['Italy', 'USA', 'Austria']
+countries = ['Italy', 'United States', 'Austria']
 pumpkins_filtered = pumpkins[pumpkins['country'].isin(countries)]
 pumpkins_filtered.to_csv('pumpkins_filtered.csv')
 
+
+##      TASK SEVEN
 #summarise mean weights by country and variety 
+# reading in filtered csv file
+pumpkins_filt = pd.read_csv('pumpkins_filtered.csv')
+#summarise mean weights by country
+country_mean = pumpkins_filt.groupby('country')['weight_lbs'].mean().to_string() 
+print(country_mean)
+#summarise mean weights by variety
+variety_mean = pumpkins_filt.groupby('variety')['weight_lbs'].mean().to_string() 
+print(variety_mean)
 
 
+##      TASK EIGHT
 #create boxplot for the three countries
+sns.set_theme(style="darkgrid")
+sns.boxplot(x = 'country', y='weight_lbs', data=pumpkins_filt, color = 'orange')
+plt.ylabel('weight(lbs)')
+plt.title('Pumpkin Weights by Country')
+plt.show()
 
 
+##      TASK NINE
 #create a faceted plot by variety 
+sns.set_theme(style='darkgrid')
+fplot = sns.catplot(x='variety', y='weight_lbs', col='country', data=pumpkins_filt, kind=
+'box', hue = 'variety', palette = 'pastel', col_wrap=3)
+fplot.set_axis_labels('Variety', 'Weight(lbs)')
+fplot.set_titles('{col_name}') #pulls the names from the dataset
+for ax in fplot.axes.flatten(): #plays with the aes of the axis
+    ax.tick_params(axis="x", rotation=90) #rotates x axis labels 
+plt.tight_layout()
+plt.show()
+
+
+
+
+
